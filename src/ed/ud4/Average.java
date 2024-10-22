08db6efa46bbcea9c59ae64130f78ab9dd1a5176
Loreto <lgonzalezc@iesch.org>
2024-10-22 18:07:03 +0200

    Changed from 50 to 100

--- src/ed/ud4/Average.java
+++ src/ed/ud4/Average.java
@@ -6,7 +6,7 @@ public class Average {
         int total = 0;
 
         for (int i = 0; i < count; i++) {
-            total += (int) (Math.random() * 50) + 1;
+            total += (int) (Math.random() * 100) + 1;
         }
         System.out.println(total);
         double media = (double) total / (double) count;
